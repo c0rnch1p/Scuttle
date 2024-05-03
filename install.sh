bfa9db6df5a5d1e0b87581b2907fef322d33ffbb
c0rNCh1p <chip@c0rnch1p.cc>
2024-05-04 03:25:50 +1000

    Update: 05/04/24 03:25

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cd .. || exit 1
 sudo cp -rf 'scuttle' '/usr/share'
 cd 'scuttle/' || exit 1
 sudo cp 'scuttle' '/bin'
-cp 'scuttle' "$HOME/.local/share/bin"
+cp 'scuttle' "$HOME/.local/share/bin" && ./'commit.sh' 2>'/dev/null'
